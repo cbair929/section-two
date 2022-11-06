4483f8aeca8a3ee02ed7768d3fdb35f17674ee45
Chris Bair <cbair929@gmail.com>
2022-11-06 12:14:32 -0500

    Commit Changes

--- Variables.java
+++ Variables.java
@@ -1,6 +1,6 @@
 public class Variables {
     public static void main(String[] args) {
-        int passengers = 20;
+        int passengers = 70;
         int busTickets = passengers;
         System.out.println(busTickets);
     }
