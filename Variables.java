33792ca29b7a2b8551a3bfe4f9f6481d1d0e6e53
Chris Bair <32574981+cbair929@users.noreply.github.com>
2022-11-06 13:51:38 -0500

    Update Variables.java

--- Variables.java
+++ Variables.java
@@ -1,6 +1,6 @@
 public class Variables {
     public static void main(String[] args) {
-        int passengers = 60;
+        int passengers = 80;
         int busTickets = passengers;
         System.out.println(busTickets);
     }
