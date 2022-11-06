c860aa7f92e44946a4cb92b30dd10fbfb49bfdc5
Chris Bair <cbair929@gmail.com>
2022-11-06 11:30:53 -0500

    Initial Commit

---
+++ JoiningStrings.java
@@ -0,0 +1,10 @@
+public class JoiningStrings {
+    public static void main(String[] args) {
+        String sentence = "His name is: ";
+        String placeholder = "Harry";
+        int points = 50;
+        String announcement = " points for Gryffindor";
+        System.out.println(sentence + placeholder);
+        System.out.println(points + announcement);
+    }
+}
